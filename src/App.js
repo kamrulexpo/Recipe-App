8c73801762ae12115b8e568e6001d6c2119437b6
Kamrul Hasan <khasancsit@gmail.com>
2020-04-17 23:37:49 +0600

    recipe app inlize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEff, useState, useEffect} from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = "cee024c9";
+  const APP_KEY = "e7264734ca91876dd9bfe3379416b400";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("")
+  const [query, setQuery] = useState('chicken');
+
+  useEffect( () => {
+    getRecipes();
+    console.log('let face data ');
+  }, [query]);
+
+  const getRecipes = async () => {
+    const exemple_req = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    const response = await fetch(exemple_req);
+    const data = await response.json();
+    console.log(data.hits);
+
+    setRecipes(data.hits)
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+
+    setSearch('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text"
+         value={search} onChange={updateSearch}/>
+        <button 
+        type="submit" className="search-button">
+          Search
+          </button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe 
+          key={recipe.recipe.label}
+          title={recipe.recipe.label} 
+          calories = {recipe.recipe.calories} 
+          image={recipe.recipe.image} 
+          ingredients={recipe.recipe.ingredients}/>
+        ))}
+      </div>
     </div>
   );
 }
